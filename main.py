472191843f2f28d13fd161e9970e074926c194df
Anamaya Sharma <sharma.011999@gmail.com>
2023-03-10 21:08:38 +0530

    Added encoding option

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def syllabus_input():
 def segregatting_units(syllabus):
     units = []
     for i in range(len(syllabus)):
-        if "Unit-I" in syllabus[i]:
+        if "Unit-I" in syllabus[i] or "Unit 1" in syllabus[i]:
             units.append(syllabus[i])
-        elif "Unit-II" in syllabus[i]:
+        elif "Unit-II" in syllabus[i] or "Unit 2" in syllabus[i]:
             units.append(syllabus[i])
-        elif "Unit-III" in syllabus[i]:
+        elif "Unit-III" in syllabus[i] or "Unit 3" in syllabus[i]:
             units.append(syllabus[i])
-        elif "Unit-IV" in syllabus[i]:
+        elif "Unit-IV" in syllabus[i] or "Unit 4" in syllabus[i]:
             units.append(syllabus[i])
     return units
 
@@ -54,7 +54,19 @@ def pretty_print(unit_wise_syllabus):
                 if k != "" :
                     print(f"\t* {k}", end="\n")
 
-def pdf_print(unit_wise_syllabus, name = "syllabus.pdf"):
+def encode(unit_wise_syllabus):
+    """This function encodes the syllabus to ignore the UnicodeEncodeError: 'latin-1' codec can't encode characters in position 334-335: ordinal not in range(256) error"""
+    for i in unit_wise_syllabus:
+        for j in range(len(unit_wise_syllabus[i])):
+            for k in range(len(unit_wise_syllabus[i][j])):
+                try:
+                    unit_wise_syllabus[i][j][k] = unit_wise_syllabus[i][j][k].encode("latin-1", "ignore").decode("latin-1")
+                except UnicodeEncodeError:
+                    print(unit_wise_syllabus[i][j][k])
+                    unit_wise_syllabus[i][j][k] = unit_wise_syllabus[i][j][k].encode("utf-8", "ignore").decode("utf-8")
+    return unit_wise_syllabus
+
+def pdf_print(unit_wise_syllabus, name = "syllabus.pdf"):    
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Times", size=16)
@@ -69,11 +81,11 @@ def pdf_print(unit_wise_syllabus, name = "syllabus.pdf"):
 
 def main():
     syllabus = syllabus_input()
-    # print(syllabus)
+    print(syllabus)
     units = segregatting_units(syllabus)
     unit_wise_syllabus = syllabus_unit_wise(units, syllabus)
     unit_wise_syllabus = processing_syllabus(unit_wise_syllabus)
-    pretty_print(unit_wise_syllabus)
+    unit_wise_syllabus = encode(unit_wise_syllabus)
     output = input("Enter the name of output file: ")
     pdf_print(unit_wise_syllabus, output)
 
